fdb6b5bb3e57a3e97f35f5aa171af8a872410f73
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-05-20 19:03:32 +0200

    add: milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, memo, useEffect } from "react";
 import axios from "axios";
+
+// componente card
+const Card = memo(({ politician }) => {
+  console.log("Render Card:", politician.name);
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h3>{politician.name}</h3>
+        <figure>
+          <img src={politician.image} alt={`Foto di ${politician.name}`} />
+        </figure>
+        <span>{politician.position}</span>
+        <p>{politician.biography}</p>
+      </div>
+    </div>
+  );
+});
 
 export default function App() {
   const url = "http://localhost:3333/politicians";
@@ -7,9 +24,11 @@ export default function App() {
   const [query, setQuery] = useState("");
 
   // richiesta get api
-  axios.get(url)
-    .then(res => setPoliticians(res.data))
-    .catch(err => console.error(err))
+  useEffect(() => {
+    axios.get(url)
+      .then(res => setPoliticians(res.data))
+      .catch(err => console.error(err))
+  }, [url])
 
   // array filtrato dopo la ricerca
   const filteredArray = useMemo(() => {
@@ -33,23 +52,9 @@ export default function App() {
         />
 
         {
-          filteredArray.map((p, i) => {
-            return (
-              <div className="card" key={i}>
-                <div className="card-content">
-                  <h3>{p.name}</h3>
-
-                  <figure>
-                    <img src={p.image} alt={`Foto di ${p.name}`} />
-                  </figure>
-
-                  <span>{p.position}</span>
-
-                  <p>{p.biography}</p>
-                </div>
-              </div>
-            )
-          })
+          filteredArray.map((p) => (
+            <Card key={p.id} politician={p} />
+          ))
         }
       </div>
     </>
