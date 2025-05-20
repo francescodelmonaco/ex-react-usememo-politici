19ace4a0b1a6b379b38cd31995054c71ef5b2c61
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-05-20 18:47:52 +0200

    add: milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import axios from "axios";
 
 export default function App() {
   const url = "http://localhost:3333/politicians";
   const [politicians, setPoliticians] = useState([]);
+  const [query, setQuery] = useState("");
 
   // richiesta get api
   axios.get(url)
     .then(res => setPoliticians(res.data))
     .catch(err => console.error(err))
 
+  // array filtrato dopo la ricerca
+  const filteredArray = useMemo(() => {
+    return politicians.filter(p => {
+      return p.name.toLowerCase().includes(query.toLowerCase()) || p.biography.toLowerCase().includes(query.toLowerCase())
+    })
+  }, [query, politicians]);
+
   return (
     <>
       <h1>Lista politici</h1>
 
+
       <div className="container">
+
+        <input
+          type="text"
+          placeholder="Cerca il nome o la biografia del politico"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+
         {
-          politicians.map((p, i) => {
+          filteredArray.map((p, i) => {
             return (
               <div className="card" key={i}>
-                <h3>{p.name}</h3>
+                <div className="card-content">
+                  <h3>{p.name}</h3>
 
-                <figure>
-                  <img src={p.image} alt={`Foto di ${p.name}`} />
-                </figure>
+                  <figure>
+                    <img src={p.image} alt={`Foto di ${p.name}`} />
+                  </figure>
 
-                <span>{p.position}</span>
+                  <span>{p.position}</span>
 
-                <p>{p.biography}</p>
+                  <p>{p.biography}</p>
+                </div>
               </div>
             )
           })
